8abc36f50997d8cd70c49b9e1dd945ddce74f7ff
StefanPotiras <96986369+StefanPotiras@users.noreply.github.com>
2022-01-30 05:45:28 -0800

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {
+            "!","!","N","N",",",",","k","k",
+            "b","b","v","v","d","d","z","z"
+        };
+        Label firstClicked, secondClicked;
+
+        public Form1()
+        {
+            InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+            if (firstClicked != null && secondClicked != null)
+                return;
+            Label clickedLabel = sender as Label;
+            if (clickedLabel == null)
+                return;
+            if (clickedLabel.ForeColor == Color.Black)
+                return;
+            if (firstClicked == null)
+            {
+                firstClicked = clickedLabel;
+                firstClicked.ForeColor = Color.Black;
+                return;
+            }
+            secondClicked = clickedLabel;
+            secondClicked.ForeColor = Color.Black;
+            CautaCastigatorul();
+
+            if(firstClicked.Text== secondClicked.Text)
+            {
+                firstClicked = null;
+                secondClicked = null;
+
+            }
+            timer1.Start();
+        }
+        private void CautaCastigatorul()
+        {
+            Label label;
+            for (int i=0;i<tableLayoutPanel1.Controls.Count;i++)
+            {
+                label = tableLayoutPanel1.Controls[i] as Label;
+                if (label != null && label.ForeColor == label.BackColor)
+                    return;
+            }
+            MessageBox.Show("Ai terminat. Esti smecher!!");
+            Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+            firstClicked = null;
+            secondClicked = null;
+
+        }
+
+        private void AssignIconsToSquares()
+        {
+            Label label;
+            int randomNumber;
+            for (int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
+            {
+                if (tableLayoutPanel1.Controls[i] is Label)
+                    label = (Label)tableLayoutPanel1.Controls[i];
+                else
+                    continue;
+                randomNumber = random.Next(0, icons.Count);
+                label.Text = icons[randomNumber];
+                icons.RemoveAt(randomNumber);
+            }
+
+        }
+    }
+}
